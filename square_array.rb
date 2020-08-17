db5ceb9ce3283364f62639cfced2641c6327700c
Adam Beatty <adam.t.beatt@gmail.com>
2020-08-17 19:30:26 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,9 @@
 def square_array(array)
   # your code here
+  square_array = []
   array.each do |element|
-    element * element
+    square_array << element*element
   end
-  return array
+
 
 end
