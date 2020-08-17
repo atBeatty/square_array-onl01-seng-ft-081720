f6e52635d55b190cf33e309c93b70510e81ff413
Adam Beatty <adam.t.beatt@gmail.com>
2020-08-17 19:30:45 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   # your code here
   square_array = []
   array.each do |element|
-    square_array << element*element
+    square_array << element * element
   end
 
 sqaure_array
